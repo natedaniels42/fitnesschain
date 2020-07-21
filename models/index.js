c8aa36b72e50e8ee505ff64d6b11ca7c7e316e85
Sabantu <Sabantuyusuf@gmail.com>
2020-07-21 16:21:47 -0500

    user model and index set up

---
+++ models/index.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/fitnessChain';
+
+mongoose.connect(MONGODB_URI, {
+	useNewUrlParser:true,
+	useCreateIndex: true,
+	useUnifiedTopology: true,
+	useFindAndModify: false,
+})
+	.then(() => console.log('connected'))
+	.catch((err) => console.log(`connection error ${err}`));
+
+
+module.exports = {
+	User: require('./User'),
+}	
